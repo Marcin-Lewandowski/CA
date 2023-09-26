3b38e936e87890804036531de5bd1676d8d416b7
Marcin Lewandowski <telfordmarcin@gmail.com>
2023-09-26 20:10:08 +0100

    Simulation update

--- oil_spread_simulation.py
+++ oil_spread_simulation.py
@@ -9,7 +9,9 @@ from matplotlib.widgets import Button
 from matplotlib.colors import ListedColormap
 
 
-# Funkcje obsługujące polecenia z menu
+global macierz
+
+# Funkcja otwiera mapę, tworzy pierwszą macierz i tworzy obraz morza, lądu  i komórek brzegowych
 def otworz_mape():
     filepath = filedialog.askopenfilename(title="Otwórz mapę")
     if filepath:
@@ -20,53 +22,45 @@ def otworz_mape():
         # Pobierz wymiary obrazu
         szerokosc, wysokosc = img.size
 
-        licznik_komorek_brzegowych = 0
-        licznik_komorek_oceanu = 0
-        licznik_komorek_ladu = 0
-        licznik_komorek_ropy = 0
-
         # Tworzenie macierzy wypełnionej zerami
-        macierz = np.zeros((wysokosc, szerokosc))
+        
+        macierz = np.empty((wysokosc, szerokosc), dtype=object)
+        #macierz = np.zeros((wysokosc, szerokosc))
 
 
-        # Iteruj przez każdy piksel i pobierz jego kolor w formacie RGB
+        # Iteruj przez każdy piksel i pobierz jego kolor w formacie RGB, tworzy macierz
         for x in range(szerokosc):
             for y in range(wysokosc):
                 pixel_color = img.getpixel((x, y))
                 if pixel_color == (0,0,255):
-                    licznik_komorek_oceanu += 1
-                    macierz[y, x] = 0.1
-                    #print("To jest ocean ", licznik)
+                    
+                    macierz[y, x] = ["S", 0]
+
                 if pixel_color == (0,255,0):
-                    licznik_komorek_ladu += 1
-                    macierz[y, x] = 0
-                    #print("To jest ląd ", licznik)
+                    
+                    macierz[y, x] = ["L"]
+                    
                 elif pixel_color == (250,200,50):
-                    licznik_komorek_brzegowych += 1
-                    macierz[y, x] = 2
-                    #print("To jest brzeg ", licznik)
+                    
+                    macierz[y, x] = ["KB", 0]
+                    
                 elif pixel_color == (0,0,0):
-                    licznik_komorek_ropy += 1
-                    macierz[y, x] = 9
-                #print(f"Piksel ({x}, {y}): RGB = {pixel_color}")
+                    
+                    macierz[y, x] = ["S", 1000]
+               
 
 
         # Zamknij plik BMP
         img.close()
+
         #Czyszczenie obszaru Canvas
         canvas.delete("all")
 
-
+        # Tworzenie obrazu morza, lądu i komórek brzegowych
         for i in range(len(macierz)):
             for j in range(len(macierz[0])):
                 value = macierz[i][j]
-
-
-
                 color = get_color(value)
-
-
-
                 canvas.create_rectangle(
                     j * cell_width,
                     i * cell_height,
@@ -75,35 +69,64 @@ def otworz_mape():
                     fill=color,
                     outline=""
                 )
-                '''
-                # Wyświetla wartość liczbową w komórce macierzy
-                canvas.create_text(
-                    j * cell_width + cell_width // 2,
-                    i * cell_height + cell_height // 2,
-                    text=str(value),
-                    fill="white" if color == "black" else "black"
-                )
-                
-                
-                
+    return macierz          
 
-        print()
-        print("Liczba komórek oceanu: ", licznik_komorek_oceanu)
-        print()
-        print("Liczba komórek lądu: ", licznik_komorek_ladu)
-        print()
-        print("Liczba komórek brzegowych: ", licznik_komorek_brzegowych)
-        print()
-        print("Liczba komórek ropy: ", licznik_komorek_ropy)
-        '''
+# Funkcja do wydruku macierzy
+def wydrukuj_macierz(macierz_gotowa):
+    for wiersz in macierz_gotowa:
+        print(wiersz)
+    print("\n")
+
+
+# Funkcja wyświetla iterację symulacji
+
+def iteracja(macierz_gotowa):
+    nowa_macierz = np.copy(macierz_gotowa)
+    wysokosc, szerokosc = macierz_gotowa.shape
+
+    for i in range(wysokosc):
+        for j in range(szerokosc):
+
+
+
+            ilosc_ropy = 0
+
+            # Obliczanie sumarycznej ilości ropy w 8 sąsiednich komórkach
+
+            for x in range(-1, 2):
+                for y in range(-1, 2):
+                    if x == 0 and y == 0:
+                        continue  # Pomijamy komórkę, którą analizujemy
+
+                    if  i + x >= 0 and i + x < wysokosc and  j + y >= 0  and j + 1 < szerokosc:
+                        ilosc_ropy = ilosc_ropy + macierz_gotowa[i + x][j + y][1]
+
+
+            nowa_macierz[i][j][1] = ilosc_ropy / 8
+
+
+            
+            #if macierz[i, j] == ["S", 0]:
+                # Symulacja rozprzestrzeniania się ropy
+                # Tutaj można dodać logikę rozprzestrzeniania się ropy na podstawie sąsiednich komórek
+                
+                # Dyfuzja
+        
+            #elif macierz[i, j] == ["KB", 0]:
+                # Symulacja rozprzestrzeniania się brzegów
+                # Tutaj można dodać logikę rozprzestrzeniania się brzegów na podstawie sąsiednich komórek
+                #pass
+
+    return nowa_macierz
+
 
 # Funkcja do przypisywania kolorów na podstawie wartości
 def get_color(value):
-    if value == 0.1:
+    if value == ["S", 0]:
         return "blue"
-    elif  value == 0:
+    elif  value == ["L"]:
         return "green"
-    elif value == 2:
+    elif value == ["KB", 0]:
         return "yellow"
     else:
         return "black"
@@ -111,6 +134,12 @@ def get_color(value):
 
 def wyjdz():
     root.quit()
+
+
+def autor():
+    pass
+
+
 
 # Tworzenie głównego okna o rozmiarze 800x600 pikseli
 root = tk.Tk()
@@ -130,109 +159,43 @@ cell_height = 4
 menu_bar = Menu(root)
 root.config(menu=menu_bar)
 
-# Tworzenie menu "Plik"
+# Tworzenie menu "Plik" i "Iteracja" i "Autor"
 plik_menu = Menu(menu_bar)
+iteracja_menu = Menu(menu_bar)
+autor_menu = Menu(menu_bar)
+
 menu_bar.add_cascade(label="Plik", menu=plik_menu)
+menu_bar.add_cascade(label="Iteracja", menu=iteracja_menu)
+menu_bar.add_cascade(label="Autor", menu=autor_menu)
 
 # Dodawanie opcji do menu "Plik"
 plik_menu.add_command(label="Otwórz mape", command=otworz_mape)
 plik_menu.add_separator()
 plik_menu.add_command(label="Wyjdź", command=wyjdz)
 
-# Uruchomienie głównej pętli programu
+
+# Dodawanie opcji do menu Iteracja
+#iteracja_menu.add_command(label="Iteracja - 1", command=krok_symulacji)
+iteracja_menu.add_separator()
+
+# Dodawanie opcji do menu Autor
+autor_menu.add_command(label="O autorze", command=autor)
+autor_menu.add_separator()
+
+
+
+
+macierz_gotowa = otworz_mape()
+
+# Wykonujemy 2 iteracji automatu komórkowego i drukujemy macierz po każdej iteracji
+for k in range(2):
+    print(f"Iteracja {k + 1}:\n")
+    wydrukuj_macierz(macierz_gotowa)
+    macierz_gotowa = iteracja(macierz_gotowa)
+
+
+
+
 root.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-
-
-# Funkcja wyświetla mapę z przypisanymi kolorami do wartości liczbowych
-def krok_symulacji(plansza):
-    nowa_plansza = np.copy(plansza)
-    for i in range(wysokosc):
-        for j in range(szerokosc):
-            sasiedzi = plansza[max(0, i-1):min(wysokosc, i+2), max(0, j-1):min(szerokosc, j+2)]
-            suma_sasiadow = np.sum(sasiedzi) - plansza[i, j]
-            if plansza[i, j] == 1:
-                if suma_sasiadow < 2 or suma_sasiadow > 3:
-                    nowa_plansza[i, j] = 0
-            else:
-                if suma_sasiadow == 3:
-                    nowa_plansza[i, j] = 1
-    return nowa_plansza
-'''
 
